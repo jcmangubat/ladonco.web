c093a202a4ad92d8c090999aa906ad1f4c1afb8d
James Mangubat <jc.mangubat@hotmail.com>
2025-08-12 01:54:00 +0800

    Update deployment configuration and base path for LadonCo project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/ladonco.web/",
   server: {
     host: "::",
     port: 8081,
